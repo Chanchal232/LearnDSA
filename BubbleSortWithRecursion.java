4d6b72a2a2e079c39a0ffd1c739cb39a21ff000e
Chanchal232 <43565538+Chanchal232@users.noreply.github.com>
2023-02-11 12:08:38 +0530

    Bubble Sort with Recusrions

---
+++ BubbleSortWithRecursion.java
@@ -0,0 +1,34 @@
+//This question is based on star pattern.
+//****
+//***
+//**
+//*
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        int[] inputarray = new int[]{5,6,4,3,2,1};
+        bubbleSort(inputarray);
+        System.out.println(Arrays.toString(inputarray));
+        
+    }
+    
+    static void bubbleSort(int[] input){
+       helper(input,0,input.length-1);
+    }
+    
+    static void helper(int[] input,int startIndex,int endIndex){
+        if(endIndex==0){
+            return;
+        }
+        if(startIndex<endIndex && input[startIndex]>input[endIndex]){
+            //swap
+            int temp = input[startIndex];
+            input[startIndex] = input[endIndex];
+            input[endIndex] = temp;
+            helper(input,startIndex+1,endIndex);
+        }else{
+            helper(input,0,endIndex-1);
+        }
+    }
+    
+}
